45bb6931c368ba925657566e4420a21efba31a02
Tariq Khoyratty <tariq.khoyratty@kennedy.ox.ac.uk>
2020-01-10 15:29:14 +0000

    Clarified required input file format, now to be specified in pipeline.yml

--- pipeline_memechip/PipelineMemechip.py
+++ pipeline_memechip/PipelineMemechip.py
@@ -76,6 +76,27 @@ db = PARAMS['database']['url'].split('./')[1]
 #######################################################################
 ########### Key helper functions ######################################
 #######################################################################
+def fetch_DataFrame(query,
+                    dbhandle=db):
+    '''Fetch query results and returns them as a pandas dataframe'''
+
+    dbhandle = sqlite3.connect(dbhandle)
+
+    cc = dbhandle.cursor()
+    sqlresult = cc.execute(query).fetchall()
+    cc.close()
+
+    # see http://pandas.pydata.org/pandas-docs/dev/generated/
+    # pandas.DataFrame.from_records.html#pandas.DataFrame.from_records
+    # this method is design to handle sql_records with proper type
+    # conversion
+
+    field_names = [d[0] for d in cc.description]
+    pandas_DataFrame = pd.DataFrame.from_records(
+        sqlresult,
+        columns=field_names)
+    return pandas_DataFrame
+
 
 def execute(queries, database=db, attach=False):
     '''Execute a list of statements sequentially'''
